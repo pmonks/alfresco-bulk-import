3f46edf9c4319467f9a5196e860f124adf30bfb6
Peter Monks <pmonks@gmail.com>
2015-10-01 19:01:07 -0700

    :white_check_mark: Tweaked large directory structure tests

--- test/data/SinglePassTests/FolderStructureTests/cleanTestData.sh
+++ test/data/SinglePassTests/FolderStructureTests/cleanTestData.sh
@@ -3,3 +3,4 @@ rm -rf deep
 rm -rf sub1
 rm -rf sub2
 rm -rf sub3
+rm -rf large-folder-structure
--- test/data/SinglePassTests/FolderStructureTests/make-large-folder-structure.clj
+++ test/data/SinglePassTests/FolderStructureTests/make-large-folder-structure.clj
@@ -1,9 +1,35 @@
 #!/usr/bin/env lein-exec
+;
+; Copyright (C) 2007-2015 Peter Monks.
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+; 
+;     http://www.apache.org/licenses/LICENSE-2.0
+; 
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+; 
+; This file is part of an unsupported extension to Alfresco.
+; 
+
+; This script creates a large number of folders with random filenames.  Each
+; folder will contain "dirs-per-dir" directories, and there are five levels of
+; nesting (so dirs-per-dir ^ 5 total directories are created).
+;
+; Dependencies:
+; * lein-exec (https://github.com/kumarshantanu/lein-exec)
+; 
+
 (require '[clojure.java.io :as io])
 (require '[clojure.string  :as s])
 
-; Be very careful changing this, as it's raised to the power 5 to come up with the final directory list
-(def dir-count 13)
+; Be very careful changing this, as it's raised to the power 5 to come up with the final directory list!
+(def dirs-per-dir 13)
 
 (def min-name-length 4)
 (def max-name-length 20)
@@ -24,13 +50,15 @@
            (nth filename-chars (rand-int filename-chars-length))))))
 
 (def directories
-  (for [a (for [i (range dir-count)] (random-filename))
-        b (for [i (range dir-count)] (random-filename))
-        c (for [i (range dir-count)] (random-filename))
-        d (for [i (range dir-count)] (random-filename))
-        e (for [i (range dir-count)] (random-filename))]
+  (for [a (for [i (range dirs-per-dir)] (random-filename))
+        b (for [i (range dirs-per-dir)] (random-filename))
+        c (for [i (range dirs-per-dir)] (random-filename))
+        d (for [i (range dirs-per-dir)] (random-filename))
+        e (for [i (range dirs-per-dir)] (random-filename))]
     (s/join "/" ["large-folder-structure" a b c d e "dummy.txt"])))
 
-(println "Creating" (count directories) "directories...")
+(println "Creating" (count directories) "random directories, 5 levels deep...")
 
 (doall (map io/make-parents directories))
+
+(println "Done")
