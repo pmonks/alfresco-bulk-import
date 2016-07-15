335688dd59fc6a79c1ac52a165fc422b6e31e341
Peter Monks <pmonks@gmail.com>
2016-07-14 22:20:57 -0700

    :art: Updated copyright notice

--- test/data/SinglePassTests/FolderStructureTests/make-large-folder-structure.clj
+++ test/data/SinglePassTests/FolderStructureTests/make-large-folder-structure.clj
@@ -1,6 +1,6 @@
 #!/usr/bin/env lein-exec
 ;
-; Copyright (C) 2007-2015 Peter Monks.
+; Copyright (C) 2007-2016 Peter Monks.
 ;
 ; Licensed under the Apache License, Version 2.0 (the "License");
 ; you may not use this file except in compliance with the License.
